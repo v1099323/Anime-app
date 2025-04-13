607db52e08e372cb465b76fbb0f3fba792dddf8e
Violett <berezovski73@gmail.com>
2025-04-13 10:35:38 +0700

    remove clerk update commit

--- providers/main-provider.tsx
+++ providers/main-provider.tsx
@@ -4,28 +4,25 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import NextTopLoader from 'nextjs-toploader'
 import { useState } from 'react'
 
-import { ProviderClerk } from './clerk-provider'
 import { ThemeProvider } from './theme-provider'
 
 export const MainProvider = ({ children }: { children: React.ReactNode }) => {
 	const [queryClient] = useState(() => new QueryClient())
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ProviderClerk>
-				<ThemeProvider
-					attribute='class'
-					defaultTheme='system'
-					enableSystem
-					disableTransitionOnChange
-				>
-					<NextTopLoader
-						color='violet'
-						height={5}
-						showSpinner={false}
-					/>
-					{children}
-				</ThemeProvider>
-			</ProviderClerk>
+			<ThemeProvider
+				attribute='class'
+				defaultTheme='system'
+				enableSystem
+				disableTransitionOnChange
+			>
+				<NextTopLoader
+					color='violet'
+					height={5}
+					showSpinner={false}
+				/>
+				{children}
+			</ThemeProvider>
 		</QueryClientProvider>
 	)
 }
