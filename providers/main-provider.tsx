8223e69df876bb88e3206d21d5d7ff734a6f8f72
Violett <berezovski73@gmail.com>
2025-04-13 09:59:40 +0700

    queryClient commit

--- providers/main-provider.tsx
+++ providers/main-provider.tsx
@@ -2,13 +2,13 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import NextTopLoader from 'nextjs-toploader'
+import { useState } from 'react'
 
 import { ProviderClerk } from './clerk-provider'
 import { ThemeProvider } from './theme-provider'
 
-const queryClient = new QueryClient()
-
 export const MainProvider = ({ children }: { children: React.ReactNode }) => {
+	const [queryClient] = useState(() => new QueryClient())
 	return (
 		<QueryClientProvider client={queryClient}>
 			<ProviderClerk>
